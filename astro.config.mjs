e590822bcd073b2fc835fb4b91e3f816c38051fc
amirs18 <amir135246@gmail.com>
2024-06-04 22:16:12 +0300

    convert to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-import deno from "@astrojs/deno";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
 import react from "@astrojs/react";
-import MillionCompiler from "@million/lint";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-supabase-auth.vercel.app",
   output: "server",
-  adapter: deno(),
+  adapter: vercel(),
   integrations: [tailwind(), react()],
 });
