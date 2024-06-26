c25dc0d64ea00c856e5a725376065ce840240435
amirs18 <amir135246@gmail.com>
2024-06-26 17:18:37 +0300

    packeges staff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,12 @@ export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+
   {
+    parserOptions: {
+      sourceType: "module",
+      globalReturn: true,
+    },
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
