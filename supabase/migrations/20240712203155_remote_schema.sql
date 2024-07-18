db15d60f243491472956e50ceb4fa0e2ade80a22
amirs18 <amir135246@gmail.com>
2024-07-18 22:06:34 +0300

    db migration order change hotfix

--- supabase/migrations/20240712203155_remote_schema.sql
+++ supabase/migrations/20240712203155_remote_schema.sql
@@ -7,6 +7,8 @@ alter table "public"."providers" add column "location" geography(Point,4326);
 
 set check_function_bodies = off;
 
+create type "public"."provider_with_longlat" as ("id" integer, "email" character varying(255), "name" character varying(255), "bio" text, "photo_link" character varying(255), "phone_number" character varying, "location_name" character varying, "availability_preferences" jsonb, "location" geography, "long" double precision, "lat" double precision);
+
 CREATE OR REPLACE FUNCTION public.get_provider_with_longlat(input_id integer)
  RETURNS SETOF provider_with_longlat
  LANGUAGE sql
@@ -15,6 +17,5 @@ select *, ST_X(public.providers.location::geometry) as long, ST_Y(public.provide
 $function$
 ;
 
-create type "public"."provider_with_longlat" as ("id" integer, "email" character varying(255), "name" character varying(255), "bio" text, "photo_link" character varying(255), "phone_number" character varying, "location_name" character varying, "availability_preferences" jsonb, "location" geography, "long" double precision, "lat" double precision);
 
 
