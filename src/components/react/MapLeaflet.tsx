fd5a537962cda3e07dccd40a253ce4671c8f541f
amirs18 <amir135246@gmail.com>
2024-07-18 21:31:27 +0300

    minor improvements to map component

--- src/components/react/MapLeaflet.tsx
+++ src/components/react/MapLeaflet.tsx
@@ -1,30 +1,46 @@
 import { LatLng, LatLngBounds } from "leaflet";
-import { useState } from "react";
+import { useEffect, useState, type ReactNode } from "react";
+import type { UseFormSetValue } from "react-hook-form";
 import { MapContainer, TileLayer, useMap, Popup, Marker } from "react-leaflet";
-import Base from "../../layout/Base.astro";
+import type { providerSchema } from "../../lib/zod.schemas";
+import z from "zod";
 
 const positionSE = new LatLng(-52.69636289886046, -142.07194463549587);
 const positionNW = new LatLng(81.46626041478547, 93.47493036450413);
 const bounds = new LatLngBounds(positionSE, positionNW);
-const style = { height: "600px", width: "80%" } as const;
-export function MapLeaflet() {
+const style = { height: 400 } as const;
+export function MapLeaflet({ children }: { children?: ReactNode }) {
   return (
     <MapContainer style={style} bounds={bounds}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MapOnClick />
+      {children}
     </MapContainer>
   );
 }
 
-function MapOnClick() {
-  const [position, setPosition] = useState<LatLng>();
+export function MapOnClick({
+  setLonglat,
+  initialLocation,
+}: {
+  setLonglat: UseFormSetValue<z.infer<typeof providerSchema>>;
+  initialLocation?: LatLng;
+}) {
+  const [position, setPosition] = useState<LatLng>(
+    initialLocation || new LatLng(0, 0),
+  );
   const map = useMap();
-  map.addEventListener("click", (e) => {
-    setPosition(e.latlng);
-  });
+  useEffect(() => {
+    map.addEventListener("click", (e) => {
+      setLonglat("location", `POINT(${e.latlng.lng} ${e.latlng.lat})`);
+      setPosition(e.latlng);
+    });
+    return () => {
+      map.removeEventListener("click");
+    };
+  }, []);
 
   return <>{position && <Marker position={position}></Marker>}</>;
 }
