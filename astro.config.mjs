99355e4660d9b0ada3cb9f30fc24ac82b341f065
amirs18 <amir135246@gmail.com>
2024-07-27 23:29:20 +0300

    only prod site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import MillionCompiler from "@million/lint";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap"
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-
-  site:
-    import.meta.env.MODE === "production"
-      ? "https://astro-supabase-apoint.vercel.app"
-      : "http://localhost:4321",
+  site: "https://astro-supabase-apoint.vercel.app",
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(),sitemap(), react()],
+  integrations: [tailwind(), sitemap(), react()],
   vite: {
     plugins: [MillionCompiler.vite()],
   },
